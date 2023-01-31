ddd3bfd4f842865ea3dc581b4763d7ec9a98d85c
Melissa Ertekin <melissaertekin@Melissa-MacBook-Pro.local>
2023-01-31 14:50:00 +0100

    Product.js uploaded

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -18,10 +18,16 @@ const Product = () => {
       <section className='product'>
         <button className='displayOption' onClick = {handleAll}>Show All</button>
         <button className='displayOption' onClick={handleCath}>Cathegories</button>
-        <div className={'container grid3'}>
+       
+        <div className={showAll ? 'container grid3' : 'hide'}>
+          {product.map((item) => (
+            <ProductBox key={item.id} id={item.id} productImg={item.productImg} name={item.name}  />
+          ))}
+        </div>
+        {/* SOR BUrayi neden olmadi  */}
+        <div className={showAll ?'hide' :'container grid3'}>
           {
             cathegories.forEach(cath => {
-              <p>{cath.name}</p>
               product.filter(item => (item.category === cath.id)).map(filteredItem => (
                 <ProductBox key={filteredItem.id} id={filteredItem.id} productImg={filteredItem.productImg} name={filteredItem.name}  />
               ))
@@ -29,12 +35,6 @@ const Product = () => {
               })
           }
         </div>
-        <div className={showAll ? 'container grid3' : 'hide'}>
-          {product.map((item) => (
-            <ProductBox key={item.id} id={item.id} productImg={item.productImg} name={item.name}  />
-          ))}
-        </div>
-        {/* SOR BUrayi neden olmadi  */}
        
 
 
