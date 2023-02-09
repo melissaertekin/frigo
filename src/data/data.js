061aa8a5b45bbc1768094333cacbd23763890a7b
Melissa Ertekin <melissaertekin@Melissa-MacBook-Pro.local>
2023-02-09 18:09:45 +0100

    tabulations fixed in data

--- src/data/data.js
+++ src/data/data.js
@@ -1,235 +1,229 @@
 export const product = [
-    {
-        id: 1,
-        name : "Potato",
-        quantity: 1,
-        category: 5001,
-        productImg: "https://media.istockphoto.com/id/647805874/fr/photo/raw-organic-baby-or-pommes-de-terre.jpg?b=1&s=170667a&w=0&k=20&c=t1rGQeQ80kiJ0RCjWnABU4C4wOHp4JddcLQ6yZoCSRI=",
-    },
-    {
-        id: 2,
-        name : "Carrot",
-        quantity: 1,
-        category: 5001,
-        productImg: "https://cdn.pixabay.com/photo/2015/03/14/14/00/carrots-673184__480.jpg",
-    },
-    {
-        id: 3,
-        name : "Broccoli",
-        quantity: 1,
-        category: 5001,
-        productImg: "https://cdn.pixabay.com/photo/2016/03/05/19/02/broccoli-1238250__480.jpg",
-    },
-    {
-        id: 4,
-        name : "Iceberg Lettuce",
-        quantity: 1,
-        category: 5001,
-        productImg: "https://media.istockphoto.com/id/1207576826/photo/iceberg-lettuce-isolated-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=7F1Bvur84rx2iypidDRPIJc67WovEsg97Y2ntxa70_Q=",
-    },
-    {
-        id: 5,
-        name : "Onion",
-        quantity: 1,
-        category: 5001,
-        productImg: "https://media.istockphoto.com/id/119472897/fr/photo/des-ampoules-doignons.jpg?b=1&s=170667a&w=0&k=20&c=YZhpVo5s_6hNqN-4Axgsp-wMHDtl0WP1Ec3eT-kZbr4=",
-    },
-    {
-        id: 6,
-        name : "Banana",
-        quantity: 1,
-        category: 5002,
-        productImg: "https://i5.walmartimages.com/asr/3bbb1151-d69a-43fb-b132-47e0bc066307.1f28c1acf3df725a6a39ba4c8738e025.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
-    },
-    {
-        id: 7,
-        name : "Strawberry",
-        quantity: 1,
-        category: 5002,
-        productImg: "https://cdn.pixabay.com/photo/2016/05/16/17/59/strawberries-1396330__480.jpg",
-    },
-    {
-        id: 8,
-        name : "Tomato",
-        quantity: 1,
-        category: 5002,
-        productImg: "https://cdn.pixabay.com/photo/2016/03/26/16/44/tomatoes-1280859__480.jpg",
-    },
-    {
-        id: 9,
-        name : "Avocado",
-        quantity: 1,
-        category: 5002,
-        productImg: "https://media.istockphoto.com/id/1354400568/fr/photo/avocat-frais-et-mûr-entier-et-coupé-en-deux-fruit-sain-isolé.jpg?b=1&s=170667a&w=0&k=20&c=mxNYzLB5jVAGoboLiqaQpuo0aknAYYjQ9fmwQp7bekE=",
-    },
-    {
-        id: 10,
-        name : "Lemon",
-        quantity: 1,
-        category: 5002,
-        productImg: "https://cdn.pixabay.com/photo/2017/02/05/12/31/lemons-2039830__480.jpg",
-    },
-    {
-        id: 11,
-        name : "Beef",
-        quantity: 1,
-        category: 5003,
-        productImg: "https://media.istockphoto.com/id/935316446/fr/photo/biftecks-de-faux-filet-cru-frais-isolés-sur-blanc.jpg?b=1&s=170667a&w=0&k=20&c=ZrtsnDmWKqco7hVew6TvdKBEXzROqrkAstgcdP5C8N0=",
-    },
-    {
-        id: 12,
-        name : "Shrimp",
-        quantity: 1,
-        category: 5003,
-        productImg: "https://cdn.pixabay.com/photo/2016/03/05/22/31/prawns-1239307__480.jpg",
-    },
-    {
-        id: 13,
-        name : "Chicken Breast",
-        quantity: 1,
-        category: 5003,
-        productImg: "https://media.istockphoto.com/id/93456470/fr/photo/deux-blanc-de-poulet-cru-sur-fond-blanc.jpg?s=612x612&w=0&k=20&c=PH90W0ZgkGCEyjKI-b49Yg0gKMjS2RlM33NcvBILRpY=",
-    },
-    {
-        id: 14,
-        name : "Salmon",
-        quantity: 1,
-        category: 5003,
-        productImg: "https://cdn.pixabay.com/photo/2018/02/08/12/41/salmon-3139390__480.jpg",
-    },
-    {
-        id: 15,
-        name : "Ground Beef",
-        quantity: 1,
-        category: 5003,
-        productImg: "https://media.istockphoto.com/id/1167764244/photo/package-of-raw-minced-meat-isolated-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=lzniq0sk15aqaRdah82PWxBC3F-20pfDVCqVeESrNGc=",
-    },
-    {
-        id: 16,
-        name : "Pasta",
-        quantity: 1,
-        category: 5004,
-        productImg: "https://img.merkandi.com/imgcache/538x420/images/offer/2022/10/08/barilla-1665260505-1665260516.png",
-    },
-    {
-        id: 17,
-        name : "Tomato Paste",
-        quantity: 1,
-        category: 5004,
-        productImg: "https://thethingswellmake.com/wp-content/uploads/2016/11/225-no-wm-easy-homemade-tomato-paste-recipe-19.jpg",
-    },
-    {
-        id: 18,
-        name : "Soy Sauce",
-        quantity: 1,
-        category: 5004,
-        productImg: "https://www.kikkoman.eu/fileadmin/_processed_/d/c/csm_201001_SS_150ml_Dispenser_EN_Vegan_WEB_f0b7d7084d.jpg",
-    },
-    {
-        id: 19,
-        name : "Bread",
-        quantity: 1,
-        category: 5004,
-        productImg: "https://cdn.pixabay.com/photo/2020/10/20/18/02/bread-5671124__480.jpg",
-    },
-    {
-        id: 20,
-        name : "Rice",
-        quantity: 1,
-        category: 5004,
-        productImg: "https://cdn.pixabay.com/photo/2019/02/15/03/28/rice-3997767__480.jpg",
-    },
-    {
-        id: 21,
-        name : "Milk",
-        quantity: 1,
-        category: 5005,
-        productImg: "https://cdn.pixabay.com/photo/2013/07/13/11/30/box-158299__480.png",
-    },
-    {
-        id: 22,
-        name : "Yogurt",
-        quantity: 1,
-        category: 5005,
-        productImg: "https://cdn.pixabay.com/photo/2013/07/13/09/50/yoghurt-156133__480.png",
-    },
-    {
-        id: 23,
-        name : "Mozarella",
-        quantity: 1,
-        category: 5005,
-        productImg: "https://cdn.pixabay.com/photo/2010/12/16/12/12/mozarella-3521__480.jpg",
-    },
-    {
-        id: 24,
-        name : "Egg",
-        quantity: 1,
-        category: 5005,
-        productImg: "https://cdn.pixabay.com/photo/2015/11/19/11/39/nest-1050964__480.jpg",
-    },
-
-
+	{
+		id: 1,
+		name : "Potato",
+		quantity: 1,
+		category: 5001,
+		productImg: "https://media.istockphoto.com/id/647805874/fr/photo/raw-organic-baby-or-pommes-de-terre.jpg?b=1&s=170667a&w=0&k=20&c=t1rGQeQ80kiJ0RCjWnABU4C4wOHp4JddcLQ6yZoCSRI=",
+	},
+	{
+		id: 2,
+		name : "Carrot",
+		quantity: 1,
+		category: 5001,
+		productImg: "https://cdn.pixabay.com/photo/2015/03/14/14/00/carrots-673184__480.jpg",
+	},
+	{
+		id: 3,
+		name : "Broccoli",
+		quantity: 1,
+		category: 5001,
+		productImg: "https://cdn.pixabay.com/photo/2016/03/05/19/02/broccoli-1238250__480.jpg",
+	},
+	{
+		id: 4,
+		name : "Iceberg Lettuce",
+		quantity: 1,
+		category: 5001,
+		productImg: "https://media.istockphoto.com/id/1207576826/photo/iceberg-lettuce-isolated-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=7F1Bvur84rx2iypidDRPIJc67WovEsg97Y2ntxa70_Q=",
+	},
+	{
+		id: 5,
+		name : "Onion",
+		quantity: 1,
+		category: 5001,
+		productImg: "https://media.istockphoto.com/id/119472897/fr/photo/des-ampoules-doignons.jpg?b=1&s=170667a&w=0&k=20&c=YZhpVo5s_6hNqN-4Axgsp-wMHDtl0WP1Ec3eT-kZbr4=",
+	},
+	{
+		id: 6,
+		name : "Banana",
+		quantity: 1,
+		category: 5002,
+		productImg: "https://i5.walmartimages.com/asr/3bbb1151-d69a-43fb-b132-47e0bc066307.1f28c1acf3df725a6a39ba4c8738e025.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+	},
+	{
+		id: 7,
+		name : "Strawberry",
+		quantity: 1,
+		category: 5002,
+		productImg: "https://cdn.pixabay.com/photo/2016/05/16/17/59/strawberries-1396330__480.jpg",
+	},
+	{
+		id: 8,
+		name : "Tomato",
+		quantity: 1,
+		category: 5002,
+		productImg: "https://cdn.pixabay.com/photo/2016/03/26/16/44/tomatoes-1280859__480.jpg",
+	},
+	{
+		id: 9,
+		name : "Avocado",
+		quantity: 1,
+		category: 5002,
+		productImg: "https://media.istockphoto.com/id/1354400568/fr/photo/avocat-frais-et-mûr-entier-et-coupé-en-deux-fruit-sain-isolé.jpg?b=1&s=170667a&w=0&k=20&c=mxNYzLB5jVAGoboLiqaQpuo0aknAYYjQ9fmwQp7bekE=",
+	},
+	{
+		id: 10,
+		name : "Lemon",
+		quantity: 1,
+		category: 5002,
+		productImg: "https://cdn.pixabay.com/photo/2017/02/05/12/31/lemons-2039830__480.jpg",
+	},
+	{
+		id: 11,
+		name : "Beef",
+		quantity: 1,
+		category: 5003,
+		productImg: "https://media.istockphoto.com/id/935316446/fr/photo/biftecks-de-faux-filet-cru-frais-isolés-sur-blanc.jpg?b=1&s=170667a&w=0&k=20&c=ZrtsnDmWKqco7hVew6TvdKBEXzROqrkAstgcdP5C8N0=",
+	},
+	{
+		id: 12,
+		name : "Shrimp",
+		quantity: 1,
+		category: 5003,
+		productImg: "https://cdn.pixabay.com/photo/2016/03/05/22/31/prawns-1239307__480.jpg",
+	},
+	{
+		id: 13,
+		name : "Chicken Breast",
+		quantity: 1,
+		category: 5003,
+		productImg: "https://media.istockphoto.com/id/93456470/fr/photo/deux-blanc-de-poulet-cru-sur-fond-blanc.jpg?s=612x612&w=0&k=20&c=PH90W0ZgkGCEyjKI-b49Yg0gKMjS2RlM33NcvBILRpY=",
+	},
+	{
+		id: 14,
+		name : "Salmon",
+		quantity: 1,
+		category: 5003,
+		productImg: "https://cdn.pixabay.com/photo/2018/02/08/12/41/salmon-3139390__480.jpg",
+	},
+	{
+		id: 15,
+		name : "Ground Beef",
+		quantity: 1,
+		category: 5003,
+		productImg: "https://media.istockphoto.com/id/1167764244/photo/package-of-raw-minced-meat-isolated-on-white-background.jpg?b=1&s=170667a&w=0&k=20&c=lzniq0sk15aqaRdah82PWxBC3F-20pfDVCqVeESrNGc=",
+	},
+	{
+		id: 16,
+		name : "Pasta",
+		quantity: 1,
+		category: 5004,
+		productImg: "https://img.merkandi.com/imgcache/538x420/images/offer/2022/10/08/barilla-1665260505-1665260516.png",
+	},
+	{
+		id: 17,
+		name : "Tomato Paste",
+		quantity: 1,
+		category: 5004,
+		productImg: "https://thethingswellmake.com/wp-content/uploads/2016/11/225-no-wm-easy-homemade-tomato-paste-recipe-19.jpg",
+	},
+	{
+		id: 18,
+		name : "Soy Sauce",
+		quantity: 1,
+		category: 5004,
+		productImg: "https://www.kikkoman.eu/fileadmin/_processed_/d/c/csm_201001_SS_150ml_Dispenser_EN_Vegan_WEB_f0b7d7084d.jpg",
+	},
+	{
+		id: 19,
+		name : "Bread",
+		quantity: 1,
+		category: 5004,
+		productImg: "https://cdn.pixabay.com/photo/2020/10/20/18/02/bread-5671124__480.jpg",
+	},
+	{
+		id: 20,
+		name : "Rice",
+		quantity: 1,
+		category: 5004,
+		productImg: "https://cdn.pixabay.com/photo/2019/02/15/03/28/rice-3997767__480.jpg",
+	},
+	{
+		id: 21,
+		name : "Milk",
+		quantity: 1,
+		category: 5005,
+		productImg: "https://cdn.pixabay.com/photo/2013/07/13/11/30/box-158299__480.png",
+	},
+	{
+		id: 22,
+		name : "Yogurt",
+		quantity: 1,
+		category: 5005,
+		productImg: "https://cdn.pixabay.com/photo/2013/07/13/09/50/yoghurt-156133__480.png",
+	},
+	{
+		id: 23,
+		name : "Mozarella",
+		quantity: 1,
+		category: 5005,
+		productImg: "https://cdn.pixabay.com/photo/2010/12/16/12/12/mozarella-3521__480.jpg",
+	},
+	{
+		id: 24,
+		name : "Egg",
+		quantity: 1,
+		category: 5005,
+		productImg: "https://cdn.pixabay.com/photo/2015/11/19/11/39/nest-1050964__480.jpg",
+	},
 ]
 
 // Cath ids starts from 5000
 export const cathegories = [
-    {
-        id: 5001,
-        name : "vegetables",
-       
-    },
-    {
-        id: 5002,
-        name : "fruits",
-        
-    },
-    {
-        id: 5003,
-        name :"meat&seafood",
-    },
-    {
-        id: 5004,
-        name : "pantry",
-    },
-    {
-        id: 5005,
-        name : "dairy",
-    }
+	{
+		id: 5001,
+		name : "vegetables",
+	},
+	{
+		id: 5002,
+		name : "fruits",
+	},
+	{
+		id: 5003,
+		name :"meat&seafood",
+	},
+	{
+		id: 5004,
+		name : "pantry",
+	},
+	{
+		id: 5005,
+		name : "dairy",
+	}
 ]
 
 export const recipes = [
-    {
-        id: 1,
-        name : "Toast",
-        recipeImg: "https://cdn.pixabay.com/photo/2016/11/29/04/00/bread-1867208__480.jpg",
-        ingrediants : [19,23],
-        desc : " Ingrediants : \n Bread, Mozarella ",
-        cook: " How to cook : blah blah blah"
-    },
-    {
-        id: 2,
-        name : "Spaghetti Bolognese",
-        recipeImg: "https://supervalu.ie/thumbnail/800x600/var/files/real-food/recipes/Uploaded-2020/spaghetti-bolognese-recipe.jpg",
-        ingrediants : [5,15,16,17],
-        desc : " Ingrediants : \n Onion, Ground Beef, Pasta, Tomato Paste",
-        cook: " How to cook : blah blah blah"
-    },
-    {
-        id: 3,
-        name : "Healty Breakfast Toast",
-        recipeImg: "https://www.cookinwithmima.com/wp-content/uploads/2021/09/Avocado-Toast-With-Eggs.jpg",
-        ingrediants : [9,19,23,24],
-        desc : " Ingrediants : \n Avocado, Bread, Mozarella, Egg ",
-        cook: " How to cook : blah blah blah"
-    },
-    {
-        id: 4,
-        name : "Sushi Bowl",
-        recipeImg: "https://healthyfitnessmeals.com/wp-content/uploads/2017/12/easy-sushi-bowls.jpg",
-        ingrediants : [9,14,18,20],
-        desc : " Ingrediants : \n Avocado, Salmon, Soy Sauce, Rice ",
-        cook: " How to cook : blah blah blah"
-    },
-
-
+	{
+		id: 1,
+		name : "Toast",
+		recipeImg: "https://cdn.pixabay.com/photo/2016/11/29/04/00/bread-1867208__480.jpg",
+		ingrediants : [19,23],
+		desc : " Ingrediants : \n Bread, Mozarella ",
+		cook: " How to cook : blah blah blah"
+	},
+	{
+		id: 2,
+		name : "Spaghetti Bolognese",
+		recipeImg: "https://supervalu.ie/thumbnail/800x600/var/files/real-food/recipes/Uploaded-2020/spaghetti-bolognese-recipe.jpg",
+		ingrediants : [5,15,16,17],
+		desc : " Ingrediants : \n Onion, Ground Beef, Pasta, Tomato Paste",
+		cook: " How to cook : blah blah blah"
+	},
+	{
+		id: 3,
+		name : "Healty Breakfast Toast",
+		recipeImg: "https://www.cookinwithmima.com/wp-content/uploads/2021/09/Avocado-Toast-With-Eggs.jpg",
+		ingrediants : [9,19,23,24],
+		desc : " Ingrediants : \n Avocado, Bread, Mozarella, Egg ",
+		cook: " How to cook : blah blah blah"
+	},
+	{
+		id: 4,
+		name : "Sushi Bowl",
+		recipeImg: "https://healthyfitnessmeals.com/wp-content/uploads/2017/12/easy-sushi-bowls.jpg",
+		ingrediants : [9,14,18,20],
+		desc : " Ingrediants : \n Avocado, Salmon, Soy Sauce, Rice ",
+		cook: " How to cook : blah blah blah"
+	},
 ]
