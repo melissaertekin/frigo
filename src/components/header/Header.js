17be2bad0b872e7b809aec3f315934dc68358aec
Melissa Ertekin <melissaertekin@Melissa-MacBook-Pro.local>
2023-02-09 18:15:14 +0100

    tabulations fixed in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,33 +4,29 @@ import { Link } from "react-router-dom";
 import MyFrigo from '../myFrigo/MyFrigo';
 import User from './User';
 import Favorites from '../recipe/Favorites';
+
 const Header = () => {
+	window.addEventListener("scroll",function() {
+			const header = this.document.querySelector(".header");
+			header.classList.toggle("active",this.window.scrollY > 100)
+	})
+	window.scrollTo({top: document.body.scrollHeight, behavior: "smooth"});
 
-    window.addEventListener("scroll",function() {
-        const header = this.document.querySelector(".header");
-        header.classList.toggle("active",this.window.scrollY > 100)
-    })
-    window.scrollTo({top: document.body.scrollHeight, behavior: "smooth"});
-
-    return (
-        
-        <header className='header'>
-            <div className='scontainer flex'>
-                <div className='logo'>
-                    <Link to='/'>
-                        <h1>Frigo</h1>
-                    </Link>
-                </div>
-                <div className='account flexCenter'>
-                    <Favorites />
-                    <MyFrigo/>
-                    <User/>
-                   
-                </div>
-            </div>
-        </header>
-
-      
-    );
+	return (
+		<header className='header'>
+			<div className='scontainer flex'>
+				<div className='logo'>
+					<Link to='/'>
+							<h1>Frigo</h1>
+					</Link>
+				</div>
+				<div className='account flexCenter'>
+					<Favorites />
+					<MyFrigo/>
+					<User/>
+				</div>
+			</div>
+		</header>
+	);
 }
 export default Header;
