310f28196b2458444be1d5c2d8f6e2b615260176
Melissa Ertekin <melissaertekin@Melissa-MacBook-Pro.local>
2023-02-09 18:25:42 +0100

    tabulations fixed in product

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -4,8 +4,8 @@ import { product, cathegories } from "../../data/data"
 import ProductBox  from "./ProductBox"
 
 const Product = () => {
-
   const [showAll, setShowAll] = useState(true);
+
   const handleAll = () => {
     setShowAll(true);
   };
@@ -14,39 +14,41 @@ const Product = () => {
   };
 
   return (
-    <>
-      <section className='product'>
-        <button className='displayOption button' onClick = {handleAll}>Show All</button>
-        <button className='displayOption button ' onClick={handleCath}>Cathegories</button>
-        <div className={showAll ? 'container grid3 ' : 'hide'}>
-          {product.map((item) => (
-            <ProductBox key={item.id} id={item.id} productImg={item.productImg} name={item.name}  />
-          ))}
-        </div>
-        {/* SOR BUrayi neden olmadi  */}
-        <div className={showAll ?'hide' :'container grid3'}>
-          {
-            cathegories.map(cath => {
-              const filteredProducts = product.filter(item => (item.category === cath.id))
-              return (
-                <div className="container">
-                  <h2>{cath.name}</h2>
-                  {filteredProducts.map(filteredItem => (
-                    <ProductBox 
-                      key={filteredItem.id} 
-                      id={filteredItem.id} 
-                      productImg={filteredItem.productImg} 
-                      name={filteredItem.name}  
-                    />
-                  ))}
-                </div>
-              )
-            })
-          }
-        </div>
-      </section>
-    </>
+    <section className='product'>
+      <button className='displayOption button' onClick = {handleAll}>Show All</button>
+      <button className='displayOption button ' onClick={handleCath}>Cathegories</button>
+      <div className={showAll ? 'container grid3 ' : 'hide'}>
+        {product.map((item) => (
+          <ProductBox 
+            key={item.id} 
+            id={item.id} 
+            productImg={item.productImg} 
+            name={item.name} 
+          />
+        ))}
+      </div>
+      <div className={showAll ?'hide' :'container grid3'}>
+      {
+        cathegories.map(cath => {
+          const filteredProducts = product.filter(item => (item.category === cath.id))
+          return (
+            <div className="container">
+              <h2>{cath.name}</h2>
+              {filteredProducts.map(filteredItem => (
+                <ProductBox 
+                  key={filteredItem.id} 
+                  id={filteredItem.id} 
+                  productImg={filteredItem.productImg} 
+                  name={filteredItem.name}  
+                />
+              ))}
+            </div>
+          )
+        })
+      }
+      </div>
+    </section>
   )
-
 }
+
 export default Product;
--- src/components/product/ProductBox.js
+++ src/components/product/ProductBox.js
@@ -1,33 +1,28 @@
-import { Link } from 'react-router-dom';
 import {AiOutlinePlusCircle} from 'react-icons/ai';
 import { useDispatch } from 'react-redux';
 import { myFrigoActions } from '../../store/myFrigoSlice';
 
-
 const ProductCart = ({ key, id, productImg, name }) => {
+	const dispatch = useDispatch();
+	
+	const addToMyFrigo = () => {
+			dispatch(myFrigoActions.addToMyFrigo({id,name , productImg}));
+	}
 
-    const dispatch = useDispatch();
-    const addToMyFrigo = () => {
-        dispatch(myFrigoActions.addToMyFrigo({id,name , productImg}));
-    }
-
-    return(
-        <>
-            <div className='box boxItem' id='product'>
-                <div className='img'>
-                    <Link>
-                        <img src={productImg} alt='productImg' />
-                    </Link>
-                 </div>
-                <div className='details'>
-                    <p>{name}</p>
-                    <button onClick={addToMyFrigo}>
-                        <AiOutlinePlusCircle/>
-                        Add To MyFrigo
-                    </button>
-                </div>
-            </div>
-        </>
-    )
+	return(
+		<div className='box boxItem' id='product'>
+			<div className='img'>
+				<img src={productImg} alt='productImg' />
+			</div>
+			<div className='details'>
+				<p>{name}</p>
+				<button onClick={addToMyFrigo}>
+						<AiOutlinePlusCircle/>
+						Add To MyFrigo
+				</button>
+			</div>
+		</div>	
+	)
 }
+
 export default ProductCart;
