6ebacd649889a95f5439781e5cd0d7778a963d3c
Melissa Ertekin <melissaertekin@Melissa-MacBook-Pro.local>
2023-02-09 18:00:09 +0100

    tabulations fixed in store

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,10 @@ import myFrigoSlice from "./myFrigoSlice";
 import recipeSlice from "./recipeSlice";
 
 const store = configureStore({
-    reducer: {
-        myfrigo: myFrigoSlice.reducer,
-        recipe: recipeSlice.reducer,
-    }
+	reducer: {
+		myfrigo: myFrigoSlice.reducer,
+		recipe: recipeSlice.reducer,
+	}
 })
 
 export default store;
--- src/store/myFrigoSlice.js
+++ src/store/myFrigoSlice.js
@@ -1,51 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const myFrigoSlice = createSlice({
-    name: "myfrigo",
-    initialState: {
-        productList: [],
-        totalQuantity: 0,
+  name: "myfrigo",
+  initialState: {
+    productList: [],
+    totalQuantity: 0,
+  },
+  reducers:{
+    addToMyFrigo(state,action){
+      const newProduct = action.payload;
+      // check if product already existe
+      const existItem = state.productList.find((product)=> product.id === newProduct.id);
+
+      if(existItem){
+        existItem.quantity++;
+      }else{
+        state.productList.push({
+          id: newProduct.id,
+          quantity: 1,
+          name: newProduct.name,
+          productImg: newProduct.productImg,
+          })
+        state.totalQuantity++;
+      }
     },
-    reducers:{
-        addToMyFrigo(state,action){
-            const newProduct = action.payload;
+    removeFromMyFrigo(state,action){
+      const chosenProduct = action.payload;
+      // check if product already existe
+      const existItem = state.productList.find((product)=> product.id === chosenProduct.id);
 
-            // check if product already existe
-            const existItem = state.productList.find((product)=> product.id === newProduct.id);
-
-            if(existItem){
-                existItem.quantity++;
-            }
-            else
-            {
-                state.productList.push({
-                    id: newProduct.id,
-                    quantity: 1,
-                    name: newProduct.name,
-                    productImg: newProduct.productImg,
-                  })
-                  state.totalQuantity++;
-            }
-
-        },
-        removeFromMyFrigo(state,action){
-            const chosenProduct = action.payload;
-
-            // check if product already existe
-            const existItem = state.productList.find((product)=> product.id === chosenProduct.id);
-
-            if(existItem.quantity === 1){
-                state.productList = state.productList.filter((item) => item.id !== chosenProduct.id)
-                state.totalQuantity --;
-            }else{
-                existItem.quantity --;
-                existItem.totalPrice -= existItem.price;
-            }
-
-        },
-    }
+      if(existItem.quantity === 1){
+        state.productList = state.productList.filter((item) => item.id !== chosenProduct.id)
+        state.totalQuantity --;
+      }else{
+        existItem.quantity --;
+        existItem.totalPrice -= existItem.price;
+      }
+    },
+  }
 })
 
 export const myFrigoActions = myFrigoSlice.actions;
-
 export default myFrigoSlice;
