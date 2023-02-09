948cdea7ac0695fb0973dd1b8fa136526ebfa57c
Melissa Ertekin <melissaertekin@Melissa-MacBook-Pro.local>
2023-02-09 18:22:10 +0100

    tabulations fixed in myFrigo

--- src/components/myFrigo/MyFrigo.js
+++ src/components/myFrigo/MyFrigo.js
@@ -1,52 +1,50 @@
 import React, {useState} from 'react';
-import {BiFridge} from "react-icons/bi";
-import {AiOutlineClose } from "react-icons/ai";
 import MyFrigoItems from './MyFrigoItems';
 import { useSelector } from 'react-redux';
+// Icon Imports 
+import {BiFridge} from "react-icons/bi";
+import {AiOutlineClose } from "react-icons/ai";
 
 const MyFrigo = () => {
-    const [myFrigoOpen, setMyFrigoOpen] = useState(false);
+	const [myFrigoOpen, setMyFrigoOpen] = useState(false);
+	const myFrigoItems = useSelector((state) => state.myfrigo.productList);
+	const quantity = useSelector((state) => state.myfrigo.totalQuantity);
 
-    const closeMyFrigo = () => {
-        setMyFrigoOpen(false);
-    };
-    const openMyFrigo = () => {
-        setMyFrigoOpen(true);
-    };
+	const closeMyFrigo = () => {
+			setMyFrigoOpen(false);
+	};
+	const openMyFrigo = () => {
+			setMyFrigoOpen(true);
+	};
+	
+	return (
+		<div className='myFrigo'>
+			<div className='myFrigo' onClick={openMyFrigo}>
+			<BiFridge className = 'myFrigoIcon'/>
+			<span className='flexCenter'>{quantity}</span>
+			</div>
+			<div className={myFrigoOpen ? "overlay" : "nonoverlay"}>
 
-    const myFrigoItems = useSelector((state) => state.myfrigo.productList);
-    const quantity = useSelector((state) => state.myfrigo.totalQuantity)
-
-
-    return (
-        <>
-           <div className='myFrigo' onClick={openMyFrigo}>
-            <BiFridge className = 'myFrigoIcon'/>
-            <span className='flexCenter'>{quantity}</span>
-           </div>
-           <div className={myFrigoOpen ? "overlay" : "nonoverlay"}>
-
-           </div>
-           <div className={myFrigoOpen ? "myFrigoItem" : "myFrigoHide"}>
-                <div className='title flex'>
-                    <h2>MyFrigo</h2>
-                    <button onClick={closeMyFrigo}>
-                        <AiOutlineClose className="icon" />                    
-                    </button>
-                </div>
-                {myFrigoItems.map((item) => (
-                    <MyFrigoItems 
-                        key={item.id}
-                        id={item.id} 
-                        productImg={item.productImg} 
-                        name={item.name} 
-                        quantity={item.quantity}
-                        />
-                ))}
-           </div>
-
-        </>
-    );
+			</div>
+			<div className={myFrigoOpen ? "myFrigoItem" : "myFrigoHide"}>
+				<div className='title flex'>
+					<h2>MyFrigo</h2>
+					<button onClick={closeMyFrigo}>
+						<AiOutlineClose className="icon" />                    
+					</button>
+				</div>
+				{myFrigoItems.map((item) => (
+					<MyFrigoItems 
+						key={item.id}
+						id={item.id} 
+						productImg={item.productImg} 
+						name={item.name} 
+						quantity={item.quantity}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default MyFrigo;
--- src/components/myFrigo/MyFrigoItems.js
+++ src/components/myFrigo/MyFrigoItems.js
@@ -4,43 +4,37 @@ import { useDispatch } from 'react-redux';
 import { myFrigoActions } from '../../store/myFrigoSlice';
 
 const MyFrigoItems = ({id,productImg,name, quantity}) => {
+	const dispatch = useDispatch();
+	const increase = () => {
+		dispatch(myFrigoActions.addToMyFrigo({id, name}))
+	}
+	const decrease = () => {
+		dispatch(myFrigoActions.removeFromMyFrigo({id}))
+	}
 
-    const dispatch = useDispatch();
-    const increase = () => {
-        dispatch(myFrigoActions.addToMyFrigo({id, name}))
-    }
-    
-    const decrease = () => {
-        dispatch(myFrigoActions.removeFromMyFrigo({id}))
-    }
-
-    return(
-        <>
-           <div className='myFrigoList' key={id}>
-            <div className='myFrigoContent'>
-                <div className='img'>
-                    <img src={productImg} alt={name}  />
-                </div>
-                <div className='details'>
-                    <p>{name}</p>
-                    <div className='price'>
-                        <div className='qty flexCenter'>
-                            <button className='plus' onClick={increase}>
-                                <AiOutlinePlus/>
-                            </button>
-                            <button className='num'>{quantity}</button>
-                            <button className='minus' onClick={decrease}>
-                                <AiOutlineMinus/>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-           </div>
-        </>
-    )
-
+	return(
+		<div className='myFrigoList' key={id}>
+			<div className='myFrigoContent'>
+					<div className='img'>
+						<img src={productImg} alt={name}  />
+					</div>
+					<div className='details'>
+						<p>{name}</p>
+						<div className='price'>
+							<div className='qty flexCenter'>
+								<button className='plus' onClick={increase}>
+										<AiOutlinePlus/>
+								</button>
+								<button className='num'>{quantity}</button>
+								<button className='minus' onClick={decrease}>
+										<AiOutlineMinus/>
+								</button>
+							</div>
+						</div>
+					</div>
+			</div>
+		</div>
+	)
 }
-
 
 export default MyFrigoItems;
